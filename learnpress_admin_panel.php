bb6d7f8d3974c1d01a4754163f5c3bb7d7d18c77
Juan Garcia <garciajj18@gmail.com>
2023-05-22 14:32:10 -0500

    Who knows

--- learnpress_admin_panel.php
+++ learnpress_admin_panel.php
@@ -25,11 +25,8 @@ function learnpress_registration_approval_admin_page()
     }
 
     // Get all users with pending approval status
-    $pending_users = get_users(array(
-        'meta_key' => 'learnpress_registration_approval_status',
-        'meta_value' => 'pending',
-    ));
 
+    $pending_users =  get_users(array('role' => 'pending_user'));
     // Display the pending registrations in a table
     echo '<div class="wrap">';
     echo '<h1>Registration Approval</h1>';
@@ -75,17 +72,12 @@ function learnpress_registration_approval_process_action()
     if (isset($_GET['action']) && current_user_can('manage_options')) {
         $action = $_GET['action'];
 
-        if ($action === 'approve' || $action === 'deny') {
-            // Get the user ID from the query parameter
-            $user_id = isset($_GET['user_id']) ? absint($_GET['user_id']) : 0;
-
-            // Update the user's approval status based on the action
-            $approval_status = ($action === 'approve') ? 'approved' : 'denied';
-            update_user_meta($user_id, 'learnpress_registration_approval_status', $approval_status);
-
-            // Redirect back to the admin page
-            wp_redirect(admin_url('edit.php?post_type=lp_course&page=learnpress-registration-approval'));
-            exit;
+        if ($action === 'approve') {
+            $user = new WP_User($user_id);
+            $user->set_role('subscriber');
+        } else if ($action === 'deny') {
+            $user = new WP_User($user_id);
+            $user->set_role('denied');
         }
     }
 }
